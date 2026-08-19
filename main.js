2446f17e94de04de992f418fd27521e9cc063460
laritsilva <tanajura.larissa@escola.pr.gov.br>
2026-08-19 16:25:25 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
-
 const botoes = document.querySelectorAll('.parametro-senha__botao');
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '!@#$*';
+const forcaSenha = document.querySelector('.forca');
 
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
+
 
 function diminuiTamanho() {
     if (tamanhoSenha > 1) {
@@ -25,17 +32,51 @@ function aumentaTamanho() {
     geraSenha();
 }
 
+for (i = 0; i < checkbox.length; i++) {
+    checkbox[i].onclick = geraSenha;
+}
 
-const campoSenha = document.querySelector('#campo-senha');
-const letrasMaiusculas = 'ABCEFGHIJKLMNOPQRSTUVWXYZ';
+
 geraSenha();
 
 function geraSenha() {
+    let alfabeto = '';
+    if (checkbox[0].checked) {
+        alfabeto = alfabeto + letrasMaiusculas;
+    }
+    if (checkbox[1].checked) {
+        alfabeto = alfabeto + letrasMinusculas;
+    }
+    if (checkbox[2].checked) {
+        alfabeto = alfabeto + numeros;
+    }
+    if (checkbox[3].checked) {
+        alfabeto = alfabeto + simbolos;
+    }
+
+
+
+
+    let senha = '';
     for (let i = 0; i < tamanhoSenha; i++) {
         let numeroAleatorio = Math.random() * letrasMaiusculas.length;
         numeroAleatorio = Math.floor(numeroAleatorio);
         console.log(letrasMaiusculas[numeroAleatorio]);
     }
+
+    campoSenha.value = letrasMaiusculas;
+    classificaSenha();
 }
 
-campoSenha.value = letrasMaiusculas;
+function classificaSenha() {
+    let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto);
+    forcaSenha.classList.remove('fraca', 'media', 'forte');
+    if (entropia > 57) {
+        forcaSenha.classList.add('forte');
+    } else if (entropia > 35 && entropia < 57) {
+        forcaSenha.classList.add('media');
+    } else if (entropia <= 35) {
+        forcaSenha.classList.add('fraca');
+    }
+
+}
